4871ef637de24a766e68cb5f346f44d017d80c5b
Jan-Niklas Braak <janniklasbraak@googlemail.com>
2015-09-22 10:21:39 +0200

    Bundled all Classes in one Library.

---
+++ include/CppRobots.hpp
@@ -0,0 +1,13 @@
+/**
+*   \copyright Copyright 2015 Jan-Niklas Braak. This project is released under
+* the MIT License, see the file
+* LICENSE.md for rights and limitations.
+*   \file CppRobots.hpp
+*   \author Jan-Niklas Braak
+*/
+
+
+#include "version.h"
+#include "Robot.hpp"
+#include "Pose.hpp"
+#include "FrameTimer.hpp"
