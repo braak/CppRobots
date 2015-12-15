cb94b446841b891f9fc8fe1c2be127c628350c93
Jan-Niklas Braak <janniklasbraak@googlemail.com>
2015-12-15 16:12:21 +0100

    Improved the Signal-Slot unit test

--- test/testSignal.cpp
+++ test/testSignal.cpp
@@ -2,32 +2,32 @@
 #include <gtest/gtest.h>
 
 TEST(SignalTest, SlotLabbda) {
-  bool wascalled1 = false;
-  Slot<> slot1([&wascalled1]() { wascalled1 = true; });
-  slot1();
-  EXPECT_TRUE(wascalled1);
+  bool wascalled = false;
+  Slot<> slot([&wascalled]() { wascalled = true; });
+  slot();
+  EXPECT_TRUE(wascalled);
 }
 
 void setTrue(bool &wasCalled) { wasCalled = true; };
 
 TEST(SignalTest, SlotFunction) {
-  bool wascalled1 = false;
+  bool wascalled = false;
 
-  Slot<bool &> slot1(setTrue);
-  slot1(wascalled1);
+  Slot<bool &> slot(setTrue);
+  slot(wascalled);
 
-  EXPECT_TRUE(wascalled1);
+  EXPECT_TRUE(wascalled);
 }
 
 TEST(SignalTest, SlotFunctor) {
-  struct ExpectEqFunctor {
+  struct SetTrueFunctor {
     bool wasCalled = false;
     void operator()() { wasCalled = true; };
   };
 
-  Slot<> slot1(ExpectEqFunctor{});
-  slot1();
-  EXPECT_TRUE(slot1.target<ExpectEqFunctor>()->wasCalled);
+  Slot<> slot(SetTrueFunctor{});
+  slot();
+  EXPECT_TRUE(slot.target<SetTrueFunctor>()->wasCalled);
 }
 
 TEST(SignalTest, SlotObserver) {
@@ -45,139 +45,144 @@ TEST(SignalTest, SlotObserver) {
 
   EXPECT_EQ(19, observer.getX());
 }
+TEST(SignalTest, SlotMultipleParameter) {
+  int val = 0;
+  Slot<int, int> slot([&val](int x, int y) { val = x + y; });
+  slot(42, 61);
+  EXPECT_EQ(103, val);
+}
 
 TEST(SignalTest, SignalConnect) {
   bool wasCalled = false;
-  Slot<> slot1([&wasCalled]() { wasCalled = true; });
-  Signal<> signal1;
-  signal1.connect(slot1);
-  signal1();
+  Slot<> slot([&wasCalled]() { wasCalled = true; });
+  Signal<> signal;
+  signal.connect(slot);
+  signal();
 
   EXPECT_TRUE(wasCalled);
 }
-
-TEST(SignalTest, SignalCall) {
-  bool wasCalled = false;
-  Slot<> slot1([&wasCalled]() { wasCalled = true; });
-  slot1();
-
-  EXPECT_TRUE(wasCalled);
+TEST(SignalTest, SignalMultipleParameter) {
+  int val = 0;
+  Slot<int, int> slot([&val](int x, int y) { val = x + y; });
+  Signal<int, int> signal;
+  signal.connect(slot);
+  signal(42, 61);
+  EXPECT_EQ(103, val);
 }
 
 TEST(SignalTest, SignalDisconnect) {
   bool wasCalled = false;
-  Slot<> slot1([&wasCalled]() { wasCalled = true; });
-  Signal<> signal1;
-  signal1.connect(slot1);
-  signal1.disconnect(slot1);
-  signal1();
+  Slot<> slot([&wasCalled]() { wasCalled = true; });
+  Signal<> signal;
+  signal.connect(slot);
+  signal.disconnect(slot);
+  signal();
 
   EXPECT_FALSE(wasCalled);
 }
 
 TEST(SignalTest, SignalDoubleDisconnect) {
   bool wasCalled = false;
-  Slot<> slot1([&wasCalled]() { wasCalled = true; });
-  Signal<> signal1;
-  signal1.connect(slot1);
-  signal1.disconnect(slot1);
-  signal1.disconnect(slot1);
-  signal1();
+  Slot<> slot([&wasCalled]() { wasCalled = true; });
+  Signal<> signal;
+  signal.connect(slot);
+  signal.disconnect(slot);
+  signal.disconnect(slot);
+  signal();
 
   EXPECT_FALSE(wasCalled);
 }
 
 TEST(SignalTest, SignalReconnect) {
   bool wasCalled = false;
-  Slot<> slot1([&wasCalled]() { wasCalled = true; });
-  Signal<> signal1;
-  signal1.connect(slot1);
-  signal1.disconnect(slot1);
-  signal1.connect(slot1);
+  Slot<> slot([&wasCalled]() { wasCalled = true; });
+  Signal<> signal;
+  signal.connect(slot);
+  signal.disconnect(slot);
+  signal.connect(slot);
 
-  signal1();
+  signal();
 
   EXPECT_TRUE(wasCalled);
 }
 
 TEST(SignalTest, SlotConnect) {
-
   bool wasCalled = false;
-  Slot<> slot1([&wasCalled]() { wasCalled = true; });
-  Signal<> signal1;
-  slot1.connect(signal1);
-  signal1();
+  Slot<> slot([&wasCalled]() { wasCalled = true; });
+  Signal<> signal;
+  slot.connect(signal);
+  signal();
 
   EXPECT_TRUE(wasCalled);
 }
 
 TEST(SignalTest, SlotDisconnect) {
   bool wasCalled = false;
-  Slot<> slot1([&wasCalled]() { wasCalled = true; });
-  Signal<> signal1;
-  signal1.connect(slot1);
-  slot1.disconnect();
-  signal1();
-
-  EXPECT_FALSE(wasCalled);
-}
-
-TEST(SignalTest, SlotDoubleDisconnect) {
-  bool wasCalled = false;
-  Slot<> slot1([&wasCalled]() { wasCalled = true; });
-  Signal<> signal1;
-  signal1.connect(slot1);
-  slot1.disconnect();
-  slot1.disconnect();
-  signal1();
-
-  EXPECT_FALSE(wasCalled);
-}
-
-TEST(SignalTest, SignalSlotDoubleDisconnect) {
-  bool wasCalled = false;
-  Slot<> slot1([&wasCalled]() { wasCalled = true; });
-  Signal<> signal1;
-  signal1.connect(slot1);
-  slot1.disconnect();
-  signal1.disconnect(slot1);
-  signal1();
-
-  EXPECT_FALSE(wasCalled);
-}
-
-TEST(SignalTest, SlotSignalDoubleDisconnect) {
-  bool wasCalled = false;
-  Slot<> slot1([&wasCalled]() { wasCalled = true; });
-  Signal<> signal1;
-  signal1.connect(slot1);
-  signal1.disconnect(slot1);
-  slot1.disconnect();
-  signal1();
+  Slot<> slot([&wasCalled]() { wasCalled = true; });
+  Signal<> signal;
+  signal.connect(slot);
+  slot.disconnect();
+  signal();
 
   EXPECT_FALSE(wasCalled);
 }
 
 TEST(SignalTest, SlotScopedDisconnect) {
   bool wasCalled = false;
-  Signal<> signal1;
+  Signal<> signal;
   {
-    Slot<> slot1([&wasCalled]() { wasCalled = true; });
-    signal1.connect(slot1);
+    Slot<> slot([&wasCalled]() { wasCalled = true; });
+    signal.connect(slot);
   }
-  signal1();
+  signal();
 
   EXPECT_FALSE(wasCalled);
 }
 
 TEST(SignalTest, SignalScopedDisconnect) {
   bool wasCalled = false;
-  Slot<> slot1([&wasCalled]() { wasCalled = true; });
+  Slot<> slot([&wasCalled]() { wasCalled = true; });
   {
-    Signal<> signal1;
-    signal1.connect(slot1);
+    Signal<> signal;
+    signal.connect(slot);
   }
-  slot1.disconnect(); // no doluble free/corruption error should be thrown
+  slot.disconnect(); // no double free/corruption error should be thrown
+}
+
+TEST(SignalTest, SlotDoubleDisconnect) {
+  bool wasCalled = false;
+  Slot<> slot([&wasCalled]() { wasCalled = true; });
+  Signal<> signal;
+  signal.connect(slot);
+  slot.disconnect();
+  slot.disconnect();
+  signal();
+
+  EXPECT_FALSE(wasCalled);
+}
+
+TEST(SignalTest, SignalSlotDoubleDisconnect) {
+  bool wasCalled = false;
+  Slot<> slot([&wasCalled]() { wasCalled = true; });
+  Signal<> signal;
+  signal.connect(slot);
+  slot.disconnect();
+  signal.disconnect(slot);
+  signal();
+
+  EXPECT_FALSE(wasCalled);
+}
+
+TEST(SignalTest, SlotSignalDoubleDisconnect) {
+  bool wasCalled = false;
+  Slot<> slot([&wasCalled]() { wasCalled = true; });
+  Signal<> signal;
+  signal.connect(slot);
+  signal.disconnect(slot);
+  slot.disconnect();
+  signal();
+
+  EXPECT_FALSE(wasCalled);
 }
 
 int main(int argc, char **argv) {
